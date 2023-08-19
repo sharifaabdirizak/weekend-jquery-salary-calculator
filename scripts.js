21cd9a0189e99d14e3d5ccfa4c4c327b91c68514
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-08-19 17:55:38 -0500

    able to see employee list on the DOM

--- scripts.js
+++ scripts.js
@@ -33,8 +33,45 @@ function submitEmployee(){
 
     //add to newEmployee object to employees array
     employees.push(newEmployee);
-    
-    
-    
 
+    // updating employee table
+    updateEmployees();
+
+}
+
+function updateEmployees() {
+    // empty exsiting array 
+    $('#currentEmployees').empty();
+
+    for (let i=0; i <employees.length; i++){
+        if (i===0){
+            $('#currentEmployees').append(`
+            <tr id="employees">
+                <td>${employees[i].firstName}</td>
+                <td>${employees[i].lastName}</td>
+                <td>${employees[i].id}</td>
+                <td>${employees[i].title}</td>
+                <td>${employees[i].annualSalary}</td>
+                <td>
+                    <button class= "deleteButton"> Delete </button>
+                </td>
+
+            </tr>
+            `)
+        } else {
+            $('#currentEmployees').append(`
+            <tr id="employees">
+                <td>${employees[i].firstName}</td>
+                <td>${employees[i].lastName}</td>
+                <td>${employees[i].id}</td>
+                <td>${employees[i].title}</td>
+                <td>${employees[i].annualSalary}</td>
+                <td>
+                    <button class= "deleteButton"> Delete </button>
+                </td>
+            </tr>
+            `)
+        }
+
+    }
 }
